4927c07a72e25e9644c561c7ac8865a55c99f419
Doc Walker <doc.walker@jameshardie.com>
2014-01-15 07:58:27 -0600

    Add specs for bash::git_setup

--- test/integration/default/serverspec/git_setup_spec.rb
+++ test/integration/default/serverspec/git_setup_spec.rb
@@ -2,7 +2,59 @@
 require 'spec_helper'
 
 describe 'bash::git_setup' do
-  it 'is pending' do
-    pending 'TODO: write this spec'
-  end # it
+  context file('/etc/gitconfig') do
+    it 'is owned by root' do
+      expect(subject).to be_owned_by('root')
+    end # it
+
+    it 'is in group root' do
+      expect(subject).to be_grouped_into('root')
+    end # it
+
+    it 'is mode 0644' do
+      expect(subject).to be_mode(644)
+    end # it
+
+    it 'matches expected content' do
+      expect(subject.content).to match(/\[alias\]/)
+    end # it
+  end # context
+
+  context file('/etc/profile.d/git-completion.sh') do
+    it 'is owned by root' do
+      expect(subject).to be_owned_by('root')
+    end # it
+
+    it 'is in group root' do
+      expect(subject).to be_grouped_into('root')
+    end # it
+
+    it 'is mode 0644' do
+      expect(subject).to be_mode(644)
+    end # it
+
+    it 'matches expected content' do
+      expect(subject.content)
+        .to match('bash/zsh completion support for core Git')
+    end # it
+  end # context
+
+  context file('/etc/profile.d/git-prompt.sh') do
+    it 'is owned by root' do
+      expect(subject).to be_owned_by('root')
+    end # it
+
+    it 'is in group root' do
+      expect(subject).to be_grouped_into('root')
+    end # it
+
+    it 'is mode 0644' do
+      expect(subject).to be_mode(644)
+    end # it
+
+    it 'matches expected content' do
+      expect(subject.content).to match('bash/zsh git prompt support')
+    end # it
+  end # context
+
 end # describe
