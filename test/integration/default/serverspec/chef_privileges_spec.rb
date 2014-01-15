ac105d6b1209844d2b13399adaeb418e20b70692
Doc Walker <doc.walker@jameshardie.com>
2014-01-15 07:16:08 -0600

    Add specs for bash::chef_privileges

--- test/integration/default/serverspec/chef_privileges_spec.rb
+++ test/integration/default/serverspec/chef_privileges_spec.rb
@@ -2,7 +2,36 @@
 require 'spec_helper'
 
 describe 'bash::chef_privileges' do
-  it 'is pending' do
-    pending 'TODO: write this spec'
-  end # it
+  context file('/etc/chef/') do
+    it 'is directory' do
+      expect(subject).to be_directory
+    end # it
+
+    it 'is owned by root' do
+      expect(subject).to be_owned_by('root')
+    end # it
+
+    it 'is in group root' do
+      expect(subject).to be_grouped_into('root')
+    end # it
+
+    it 'is mode 755' do
+      expect(subject).to be_mode(755)
+    end # it
+  end # context
+
+  context file('/etc/chef/encrypted_data_bag_secret') do
+    it 'is owned by root' do
+      expect(subject).to be_owned_by('root')
+    end # it
+
+    it 'is in group root' do
+      expect(subject).to be_grouped_into('root')
+    end # it
+
+    it 'is mode 0600' do
+      expect(subject).to be_mode(600)
+    end # it
+  end # context
+
 end # describe
