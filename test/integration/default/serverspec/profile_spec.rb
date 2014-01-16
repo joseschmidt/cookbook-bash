b0bafc591df7b7a322d4ba345b8bf2a17121249b
Doc Walker <doc.walker@jameshardie.com>
2014-01-15 21:06:56 -0600

    Add specs for bash::profile

--- test/integration/default/serverspec/profile_spec.rb
+++ test/integration/default/serverspec/profile_spec.rb
@@ -2,7 +2,21 @@
 require 'spec_helper'
 
 describe 'bash::profile' do
-  it 'is pending' do
-    pending 'TODO: write this spec'
-  end # it
+  context file('/etc/profile.d/jhx_profile.sh') do
+    it 'is owned by root' do
+      expect(subject).to be_owned_by('root')
+    end # it
+
+    it 'is in group root' do
+      expect(subject).to be_grouped_into('root')
+    end # it
+
+    it 'is mode 0644' do
+      expect(subject).to be_mode(644)
+    end # it
+
+    it 'matches expected content' do
+      expect(subject.content).to match('# fake')
+    end # it
+  end # context
 end # describe
