8f319acbcd4152b0250f48729217ac36ed165164
Doc Walker <doc.walker@jameshardie.com>
2014-01-15 20:59:54 -0600

    Add specs for bash::path_usr_sbin

--- test/integration/default/serverspec/path_usr_sbin_spec.rb
+++ test/integration/default/serverspec/path_usr_sbin_spec.rb
@@ -2,7 +2,9 @@
 require 'spec_helper'
 
 describe 'bash::path_usr_sbin' do
-  it 'is pending' do
-    pending 'TODO: write this spec'
-  end # it
+  context file('/etc/profile.d/path_usr_sbin.sh') do
+    it 'is not a file' do
+      expect(subject).to_not be_file
+    end # it
+  end # context
 end # describe
