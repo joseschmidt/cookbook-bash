c58c14fa0f64dc3e6c3ecbb68bec6fa3fff88e68
Doc Walker <doc.walker@jameshardie.com>
2014-01-15 21:16:10 -0600

    Add specs for bash::rmate

--- test/integration/default/serverspec/rmate_spec.rb
+++ test/integration/default/serverspec/rmate_spec.rb
@@ -2,7 +2,21 @@
 require 'spec_helper'
 
 describe 'bash::rmate' do
-  it 'is pending' do
-    pending 'TODO: write this spec'
-  end # it
+  context file('/usr/local/bin/rmate') do
+    it 'is owned by root' do
+      expect(subject).to be_owned_by('root')
+    end # it
+
+    it 'is in group root' do
+      expect(subject).to be_grouped_into('root')
+    end # it
+
+    it 'is mode 0755' do
+      expect(subject).to be_mode(755)
+    end # it
+
+    it 'matches expected content' do
+      expect(subject.content).to match('rmate version 1.5 \(2012-09-19\)')
+    end # it
+  end # context
 end # describe
