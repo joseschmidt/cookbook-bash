4b0f27abf25ebd2d944db1c323cd416bd4ed1607
Doc Walker <doc.walker@jameshardie.com>
2014-02-11 05:52:32 -0500

    Fix #61 use node.set[]= notation

--- spec/profile_spec.rb
+++ spec/profile_spec.rb
@@ -4,9 +4,7 @@ require 'spec_helper'
 describe 'bash::profile' do
   let(:chef_run) do
     ChefSpec::Runner.new do |node|
-      node.set['file'] = {
-        'header' => 'node.file.header'
-      }
+      node.set['file']['header'] = 'node.file.header'
     end.converge(described_recipe)
   end # let
 
